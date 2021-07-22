eef018f277a5eb265f69358cb51113005c0bbfe7
=yennle <=phiyen263@gmail.com>
2021-07-22 12:47:23 -0400

    change test

--- src/containers/Settings/Settings.js
+++ src/containers/Settings/Settings.js
@@ -1,11 +1,11 @@
-import React from "react";
-import "./Settings.css";
+import React from 'react';
+import './Settings.css';
 
 export default function Settings() {
   return (
-    <div className="Home">
-      <div className="lander">
-        <h1>Settings page</h1>
+    <div className='Home'>
+      <div className='lander'>
+        <h1>Settings</h1>
       </div>
     </div>
   );
