255f72a326cddd57b26dae0c2400d986f8a46b13
=yennle <=phiyen263@gmail.com>
2021-08-03 12:58:43 -0400

    Home Page

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,31 +1,51 @@
-import React from "react";
-import { useAppContext } from "../../libs/contextLib";
-import "./Home.css";
-
-
-
+import React from 'react';
+import { Row, Col, Card } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { useAppContext } from '../../libs/contextLib';
+import './Home.css';
+import Slider from './Slider';
 export default function Home() {
   const { isAuthenticated } = useAppContext();
 
   function renderLander() {
     return (
-      <div className="lander">
+      <div className='lander m-0'>
         <h1>Home Medical Care</h1>
-        <p className="text-muted">Cloud Computing Project</p>
+        <p className='text-muted'>Cloud Computing Project</p>
       </div>
     );
   }
 
   function renderGreeting() {
     return (
-      <div className="lander">
-        <h1>Welcome</h1>
+      <div className='lander'>
+        <Slider />
+        <Row className='mt-5'>
+          <Col md={12} xl={6}>
+            <LinkContainer className='text-center' to='/documents/new'>
+              <Card className=' document-card text-white'>
+                <Card.Body>
+                  <span className='h3'>Import Medical Document</span>
+                </Card.Body>
+              </Card>
+            </LinkContainer>
+          </Col>
+          <Col md={12} xl={6}>
+            <LinkContainer className='text-center' to='/symptoms/new'>
+              <Card className='document-card text-white'>
+                <Card.Body>
+                  <span className='h3'>Report Symptoms</span>
+                </Card.Body>
+              </Card>
+            </LinkContainer>
+          </Col>
+        </Row>
       </div>
     );
   }
 
   return (
-    <div className="Home">
+    <div className='Home'>
       {isAuthenticated ? renderGreeting() : renderLander()}
     </div>
   );
---
+++ src/containers/Home/Slider.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { Carousel } from 'react-bootstrap';
+import slider1 from './slider1.jpg';
+import './Home.css';
+export default function Slider() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <img
+          className='d-block w-100 image-slider'
+          src={slider1}
+          alt='First slide'
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img
+          className='d-block w-100 image-slider'
+          src={slider1}
+          alt='Second slide'
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className='d-block w-100 image-slider'
+          src={slider1}
+          alt='Third slide'
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
