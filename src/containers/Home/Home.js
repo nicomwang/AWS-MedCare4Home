e9daa683fdab678dea09f971642425f955de8b73
wangm1atwit <nicomwang@gmail.com>
2021-08-04 23:37:08 -0400

    Updated Home

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -31,71 +31,71 @@ export default function Home() {
         <h1 className=" title1 text-center mt-5">
           Quick Access to Your Services
         </h1>
-        <div className ="service-cards">
-        <Row>
-          <Col md={12} lg={6} xl={6}>
-            <LinkContainer className="text-center" to="/documents/new">
-              <Card className=" home-card left-card mt-5">
-                <Card.Title className="mt-4 service">
-                  <GoFile size={28} className="icon mr-1" /> Upload Medical
-                  Documents
-                </Card.Title>
-                <Card.Body>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Body>
-              </Card>
-            </LinkContainer>
-          </Col>
-          <Col md={12} lg={6} xl={6}>
-            <LinkContainer className="text-center" to="/symptoms/new">
-              <Card className="home-card right-card mt-5">
-                <Card.Title className="mt-4">
-                  <GoReport size={28} className="mr-3" />
-                  Report Symptom
-                </Card.Title>
-                <Card.Body>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Body>
-              </Card>
-            </LinkContainer>
-          </Col>
-        </Row>
-        <Row >
-          <Col md={12} lg={6} xl={6}>
-            <LinkContainer className="text-center" to="/">
-              <Card className=" home-card left-card mt-5">
-                <Card.Title className="icon mt-4">
-                  <MdAlarm size={28} className="mr-2" />
-                  Setup Medication Reminder
-                  <br />
-                  (Comming Soon)
-                </Card.Title>
-                <Card.Body>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Body>
-              </Card>
-            </LinkContainer>
-          </Col>
-          <Col md={12} lg={6} xl={6}>
-            <LinkContainer className="text-center" to="/">
-              <Card className="home-card right-card mt-5">
-                <Card.Title className="mt-4">
-                  <FiUsers size={25} className="mr-3" />
-                  Manage Family Profile <br />
-                  (Comming Soon)
-                </Card.Title>
-                <Card.Body>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Body>
-              </Card>
-            </LinkContainer>
-          </Col>
-        </Row>
-      </div>
+        <div className="service-cards">
+          <Row>
+            <Col md={12} lg={6} xl={6}>
+              <LinkContainer className="text-center" to="/documents/new">
+                <Card className=" home-card left-card mt-5">
+                  <Card.Title className="mt-4 service">
+                    <GoFile size={28} className="icon mr-1" /> Upload Medical
+                    Documents
+                  </Card.Title>
+                  <Card.Body>
+                    Upload electronic file of your medical documents. <br />
+                    It can be a scan or clear photo. Maximum file size: 5MB.
+                  </Card.Body>
+                </Card>
+              </LinkContainer>
+            </Col>
+            <Col md={12} lg={6} xl={6}>
+              <LinkContainer className="text-center" to="/symptoms/new">
+                <Card className="home-card right-card mt-5">
+                  <Card.Title className="mt-4">
+                    <GoReport size={28} className="mr-3" />
+                    Report Symptom
+                  </Card.Title>
+                  <Card.Body>
+                    Identify and self-report symptoms that you are experiencing
+                    to keep track of potential medical issues.
+                  </Card.Body>
+                </Card>
+              </LinkContainer>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={12} lg={6} xl={6}>
+              <LinkContainer className="text-center" to="/">
+                <Card className=" home-card left-card mt-5">
+                  <Card.Title className="icon mt-4">
+                    <MdAlarm size={28} className="mr-2" />
+                    Setup Medication Reminder
+                    <br />
+                    (Comming Soon)
+                  </Card.Title>
+                  <Card.Body>
+                    Set up personalized reminders to make adhering to your
+                    medication regimen easy.
+                  </Card.Body>
+                </Card>
+              </LinkContainer>
+            </Col>
+            <Col md={12} lg={6} xl={6}>
+              <LinkContainer className="text-center" to="/">
+                <Card className="home-card right-card mt-5">
+                  <Card.Title className="mt-4">
+                    <FiUsers size={25} className="mr-3" />
+                    Manage Family Profile <br />
+                    (Comming Soon)
+                  </Card.Title>
+                  <Card.Body>
+                    Manage healthcare data for you and your loved ones by
+                    switching between profiles.
+                  </Card.Body>
+                </Card>
+              </LinkContainer>
+            </Col>
+          </Row>
+        </div>
       </div>
     );
   }
